2f50f25d9d605f4fa547e5ffe5789ad745b4357b
ksamrat224 <ksamrat224@gmail.com>
2025-03-11 21:23:51 +0545

    youtube api fetched

---
+++ src/utils/constants.jsx
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyCB7S-atQd90ECLe6T2XWzL4Kzy1kMgp68";
+
+export const YOUTUBE_VIDEOS_API =
+  " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  GOOGLE_API_KEY;
