2452531542a68fd7971379a2a2c380060e43e36c
ksamrat224 <ksamrat224@gmail.com>
2025-03-11 20:46:23 +0545

    buttons completed

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import Button from "./Button";
+//  const List=["all","live"] do mapping
 const ButtonList = () => {
   return (
-    <div>ButtonList</div>
-  )
-}
+    <div className="flex">
+      <Button name="All" />
+      <Button name="Gaming" />
+      <Button name="Music" />
+      <Button name="Mixes" />
+      <Button name="Premier League" />
+      <Button name="JavaScript" />
+      <Button name="Algorithms" />
+      <Button name="Dinners" />
+      <Button name="Masala Films" />
+      <Button name="Indie Rocks" />
+      <Button name="Stadiums" />
+    </div>
+  );
+};
 
-export default ButtonList
+export default ButtonList;
