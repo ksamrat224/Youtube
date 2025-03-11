2f40f467a34c267755b320eedc4a81a1c8eb880d
ksamrat224 <ksamrat224@gmail.com>
2025-03-11 20:06:33 +0545

    side bar toggle created

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,31 +1,47 @@
-const Head =()=>{
-    return (
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+
+const Head = () => {
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+  return (
     <div className=" grid grid-flow-col p-5 m-2 shadow-lg">
       <div className=" flex col-span-1  ">
-         <img
+        <img
+          onClick={() => toggleMenuHandler()}
           className="h-8 cursor-pointer"
           src="https://www.svgrepo.com/show/312300/hamburger-menu.svg"
           alt="Menu"
         />
-         <img
-          className="h-8 mx-2 "
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/768px-YouTube_Logo_2017.svg.png"
-          alt="youtube-logo"
+        <a href="">
+          <img
+            className="h-8 mx-2 "
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/768px-YouTube_Logo_2017.svg.png"
+            alt="youtube-logo"
           />
+        </a>
       </div>
 
-       <div className="col-span-10 px-10 ">
-        <input className="w-2/3 border border-gray-700  p-2 rounded-l-full" type="text" placeholder="See What You Love" />  
-        <button className="border border-gray-400 p-2 rounded-r-full bg-gray-200">Search🔍</button>
-       </div>
-       <div className="col-span-1">
-       <img
+      <div className="col-span-10 px-10 ">
+        <input
+          className="w-2/3 border border-gray-700  p-2 rounded-l-full"
+          type="text"
+          placeholder="See What You Love"
+        />
+        <button className="border border-gray-400 p-2 rounded-r-full bg-gray-200">
+          Search🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
           className="h-8"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s"
           alt="User-Icon"
-        /> 
-       </div>
+        />
+      </div>
     </div>
-    )
-}
-export default Head
+  );
+};
+export default Head;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 export const SideBar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+  //Early Return Pattern
+  if (!isMenuOpen) return null;
   return (
-    <div className='p-5 shadow-lg col-span-1 w-48'>
-       <ul>
+    <div className="p-5 shadow-lg col-span-1 w-48">
+      <ul>
         <li>Home</li>
         <li>Shorts</li>
         <li>videos</li>
         <li>Live</li>
       </ul>
-      <h1 className='font-bold pt-5'>Subscriptions</h1>
+      <h1 className="font-bold pt-5">Subscriptions</h1>
       <ul>
         <li>Music</li>
         <li>Sports</li>
         <li>Gaming</li>
         <li>Movies</li>
       </ul>
-      <h1 className='font-bold pt-5'>Watch Later</h1>
+      <h1 className="font-bold pt-5">Watch Later</h1>
       <ul>
         <li>Music</li>
         <li>Sports</li>
@@ -24,6 +28,6 @@ export const SideBar = () => {
         <li>Movies</li>
       </ul>
     </div>
-  )
-}
-export default SideBar
+  );
+};
+export default SideBar;
