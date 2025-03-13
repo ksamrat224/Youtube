dbd71aa25a0b0bbdf49d7088f9acf9d5fba350c5
ksamrat224 <ksamrat224@gmail.com>
2025-03-13 21:17:15 +0545

    fetching data

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+  //updating the state so it triggers the
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    getVideos();
+  }, []);
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    console.log(json.items);
+    setVideos(json.items);
+  };
+  return (
+    <div>
+      <VideoCard info={videos[0]} />
+    </div>
+  );
 };
 
 export default VideoContainer;
