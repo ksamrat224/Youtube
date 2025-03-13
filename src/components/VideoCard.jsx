d90f881d10dd954649d704758758fb178eb37ac8
ksamrat224 <ksamrat224@gmail.com>
2025-03-13 22:02:58 +0545

    video card making

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+const VideoCard = ({ info }) => {
+  console.log(info);
+  const { snippet, statistics } = info;
+  const { channelTitle, title, thumbnails } = snippet;
+
+  return (
+    <div className="p-2 m-2 w-72 shadow-lg bg-gray-200">
+      <img className="rounded-md" src={thumbnails.medium.url} alt="ypicture" />
+      <ul>
+        <li className="font-bold py-2 ">{title}</li>
+        <li>{channelTitle}</li>
+        <li>{statistics.viewCount} views</li>
+      </ul>
+    </div>
+  );
 };
 
 export default VideoCard;
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -11,12 +11,13 @@ const VideoContainer = () => {
   const getVideos = async () => {
     const data = await fetch(YOUTUBE_VIDEOS_API);
     const json = await data.json();
-    console.log(json.items);
     setVideos(json.items);
   };
   return (
     <div>
-      <VideoCard info={videos[0]} />
+      {videos.map((video) => (
+        <VideoCard key={video.id} info={video} />
+      ))}
     </div>
   );
 };
