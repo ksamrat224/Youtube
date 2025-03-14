a91f23af8add05772b7ff179298e8f4f777fa80d
ksamrat224 <ksamrat224@gmail.com>
2025-03-14 09:34:27 +0545

    react router dom installed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,12 @@
-import MainContainer from "./MainContainer"
-import  SideBar  from "./SideBar"
+import { Outlet } from "react-router-dom";
+import SideBar from "./SideBar";
 
-const Body=()=> {
-    return(
-        <div className="flex">
-        <SideBar/>
-        <MainContainer/>
-        </div>
-    )
-}
-export default Body
+const Body = () => {
+  return (
+    <div className="flex">
+      <SideBar />
+      <Outlet />
+    </div>
+  );
+};
+export default Body;
