7b4066236413b21aebcc8cea48e52b4f2c961f8d
ksamrat224 <ksamrat224@gmail.com>
2025-03-17 21:51:26 +0545

    comment in making

--- src/components/CommentsContainer.jsx
+++ src/components/CommentsContainer.jsx
@@ -1,5 +1,11 @@
-import react from "react";
+import React from "react";
+
 const CommentsContainer = () => {
-  return <div></div>;
+  return (
+    <div className="m-5 p-2">
+      <h1 className="font-bold text-2xl">Comments:</h1>
+    </div>
+  );
 };
+
 export default CommentsContainer;
--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -11,8 +12,8 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
-    <>
-      <div className=" px-5 py-5">
+    <div className="flex flex-col">
+      <div className=" px-5">
         <iframe
           width="1000"
           height="500"
@@ -24,7 +25,8 @@ const WatchPage = () => {
           allowfullscreen
         ></iframe>
       </div>
-    </>
+      <CommentsContainer />
+    </div>
   );
 };
 
