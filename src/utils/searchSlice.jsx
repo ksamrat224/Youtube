958c1e755232d3d78ab012cdbde1476cf5655629
ksamrat224 <ksamrat224@gmail.com>
2025-03-17 19:37:06 +0545

    searchSlice for caching

---
+++ src/utils/searchSlice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const searchSlice = createSlice({
+  name: "search",
+  initialState: {},
+  reducers: {
+    cacheResults: (state, action) => {
+      state = { ...action.payload, ...state }; //spread operator
+    },
+  },
+});
+export const { cacheResults } = searchSlice.actions;
+export default searchSlice.reducer;
