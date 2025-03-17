1259217693c48c2f76306e26412684b303a6695f
ksamrat224 <ksamrat224@gmail.com>
2025-03-17 21:43:11 +0545

    CommentContainer

---
+++ src/components/CommentsContainer.jsx
@@ -0,0 +1,5 @@
+import react from "react";
+const CommentsContainer = () => {
+  return <div></div>;
+};
+export default CommentsContainer;
--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -11,18 +11,20 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className=" px-5 py-5">
-      <iframe
-        width="1000"
-        height="500"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
-    </div>
+    <>
+      <div className=" px-5 py-5">
+        <iframe
+          width="1000"
+          height="500"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </>
   );
 };
 
