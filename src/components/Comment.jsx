57c751576bea28343a7cff2d30c27bcccb1bb073
ksamrat224 <ksamrat224@gmail.com>
2025-03-18 18:10:51 +0545

    nested comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Comment = ({ data }) => {
   const { name, text, replies } = data;
   return (
-    <div className="flex shadow-lg bg-gray-200 p-2 rounded-lg">
+    <div className="flex shadow-lg bg-gray-200 p-2 rounded-lg my-2">
       <img
         className="w-12 h-12"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s"
@@ -20,7 +20,12 @@ const Comment = ({ data }) => {
 export const CommentList = ({ comments }) => {
   //don't use indexes as keys!!disclaimer
   return comments.map((comment, index) => (
-    <Comment key={index} data={comment} />
+    <div>
+      <Comment key={index} data={comment} />
+      <div className="pl-5 border-l-2 border-l-black ml-5">
+        <CommentList comments={comment.replies} />
+      </div>
+    </div>
   ));
 };
 
