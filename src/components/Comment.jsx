734e7cc490a65261b4f0de7f461000fbb8110b7f
ksamrat224 <ksamrat224@gmail.com>
2025-03-18 17:34:46 +0545

    Comment container and comment data

---
+++ src/components/Comment.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Comment = ({ data }) => {
+  return <div>Comment</div>;
+};
+
+export default Comment;
--- src/components/CommentsContainer.jsx
+++ src/components/CommentsContainer.jsx
@@ -1,9 +1,68 @@
 import React from "react";
+import Comment from "./Comment";
+
+const commentsData = [
+  {
+    name: "samrat karki",
+    text: "lorem ipsum dolor sit amet",
+    replies: [],
+  },
+  {
+    name: "samrat karki",
+    text: "lorem ipsum dolor sit amet",
+    replies: [
+      {
+        name: "samrat karki",
+        text: "lorem ipsum dolor sit amet",
+        replies: [],
+      },
+      {
+        name: "samrat karki",
+        text: "lorem ipsum dolor sit amet",
+        replies: [],
+      },
+      {
+        name: "samrat karki",
+        text: "lorem ipsum dolor sit amet",
+        replies: [
+          {
+            name: "samrat karki",
+            text: "lorem ipsum dolor sit amet",
+            replies: [
+              {
+                name: "samrat karki",
+                text: "lorem ipsum dolor sit amet",
+                replies: [
+                  {
+                    name: "samrat karki",
+                    text: "lorem ipsum dolor sit amet",
+                    replies: [],
+                  },
+                  {
+                    name: "samrat karki",
+                    text: "lorem ipsum dolor sit amet",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "samrat karki",
+    text: "lorem ipsum dolor sit amet",
+    replies: [],
+  },
+];
 
 const CommentsContainer = () => {
   return (
     <div className="m-5 p-2">
       <h1 className="font-bold text-2xl">Comments:</h1>
+      <Comment data={commentsData[0]} />
     </div>
   );
 };
