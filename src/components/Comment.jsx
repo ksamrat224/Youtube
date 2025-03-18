cb38a9ebb346d868f2db5f1b5dacc7b9d084abce
ksamrat224 <ksamrat224@gmail.com>
2025-03-18 17:43:11 +0545

    comment name and text with css

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Comment = ({ data }) => {
-  return <div>Comment</div>;
+  const { name, text, replies } = data;
+  return (
+    <div className="flex shadow-lg bg-gray-200 p-2 rounded-lg">
+      <img
+        className="w-12 h-12"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s"
+        alt="User"
+      />
+      <div>
+        <p className="font-bold">Name:{name}</p>
+        <p>Comment:{text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Comment;
